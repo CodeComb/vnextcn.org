0b9dd3488e7a647e5080d3fd711b40dd23fdd65c
あまみや ゆうこ <1@1234.sh>
2015-10-21 11:28:17 +0800

    Add tag model

--- src/CodeComb.vNextExperimentCenter/Models/CenterContext.cs
+++ src/CodeComb.vNextExperimentCenter/Models/CenterContext.cs
@@ -9,15 +9,15 @@ namespace CodeComb.vNextExperimentCenter.Models
 {
     public class CenterContext : IdentityDbContext<User, IdentityRole<long>, long>
     {
-        //public DbSet<Problem> Problems { get; set; }
-        //public DbSet<Status> Statuses { get; set; }
-        //public DbSet<Blob> Blobs { get; set; }
+        public DbSet<Problem> Problems { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Blob> Blobs { get; set; }
+        public DbSet<Tag> Tags { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-            /*
             builder.Entity<Problem>(e =>
             {
                 e.HasIndex(x => x.Title);
@@ -37,7 +37,12 @@ namespace CodeComb.vNextExperimentCenter.Models
                 e.HasIndex(x => x.ContentLength);
                 e.HasIndex(x => x.ContentType);
             });
-            */
+            
+            builder.Entity<Tag>(e =>
+            {
+               e.HasIndex(x => x.PRI);
+               e.HasIndex(x => x.Title); 
+            });
         }
     }
 }
