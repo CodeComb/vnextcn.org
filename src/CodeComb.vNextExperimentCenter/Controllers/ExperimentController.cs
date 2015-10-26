96526b304411fd5bedae3278d8ced8cc0ce808e8
あまみや ゆうこ <1@1234.sh>
2015-10-26 17:16:42 +0800

    Show experiment

--- src/CodeComb.vNextExperimentCenter/Controllers/ExperimentController.cs
+++ src/CodeComb.vNextExperimentCenter/Controllers/ExperimentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using CodeComb.vNextExperimentCenter.Models;
 
 namespace CodeComb.vNextExperimentCenter.Controllers
 {
@@ -10,7 +11,33 @@ namespace CodeComb.vNextExperimentCenter.Controllers
     {
         public IActionResult Index()
         {
-            return AjaxPagedView(DB.Problems, ".lst-experiments", 100);
+            IEnumerable<Problem> ret = DB.Problems;
+            if (!(User.IsInRole("Root") || User.IsInRole("Manager")))
+                ret = ret.Where(x => x.CheckPassed);
+            return AjaxPagedView(ret, ".lst-experiments", 100);
+        }
+        
+        [Route("Experiment/{id:long}")]
+        public IActionResult Show(long id)
+        {
+            var exp = DB.Problems
+                .Where(x => x.Id == id)
+                .SingleOrDefault();
+            if (exp == null)
+                return Prompt(x => 
+                {
+                    x.Title = "资源没有找到";
+                    x.Details = "您请求的资源没有找到，请返回重试！";
+                    x.StatusCode = 404;
+                });
+            if (!(User.IsInRole("Root") || User.IsInRole("Manager")) && exp.CheckPassed == false)
+                return Prompt(x => 
+                {
+                    x.Title = "资源没有找到";
+                    x.Details = "您请求的资源没有找到，请返回重试！";
+                    x.StatusCode = 404;
+                });
+            return View(exp);
         }
     }
 }
