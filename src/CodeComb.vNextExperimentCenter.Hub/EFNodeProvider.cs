0751649ae57425dc84f10b6c0427e10a5f9efaed
あまみや ゆうこ <1@1234.sh>
2015-10-28 09:51:29 +0800

    Judge hub & client

---
+++ src/CodeComb.vNextExperimentCenter.Hub/EFNodeProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeComb.vNextExperimentCenter.Hub.Models;
+
+namespace CodeComb.vNextExperimentCenter.Hub
+{
+    public class EFNodeProvider<TContext> : INodeProvider
+        where TContext : INodeDbContext
+    {
+        private TContext DB;
+
+        public EFNodeProvider(TContext db)
+        {
+            DB = db;
+        }
+
+        public List<Node> GetNodes()
+        {
+            var ret = new List<Node>();
+            foreach(var x in DB.Nodes.ToList())
+            {
+                var y = new Node
+                {
+                    Server = x.Server,
+                    Port = x.Port,
+                    CurrentThread = 0,
+                    MaxThread = 0,
+                    PrivateKey = x.PrivateKey,
+                    LostConnectionCount = 0
+                };
+                ret.Add(y);
+                y.Init();
+            }
+            return ret;
+        }
+    }
+}
---
+++ src/CodeComb.vNextExperimentCenter.Hub/Node.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeComb.vNextExperimentCenter.Hub
+{
+    public enum NodeStatus
+    {
+        Free,
+        Working,
+        Busy,
+        Lost
+    }
+
+    public class Node
+    {
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public int MaxThread { get; set; }
+        public int CurrentThread { get; set; }
+        public int LostConnectionCount { get; set; } = 0;
+        public string PrivateKey { get; set; }
+        public NodeStatus Status
+        {
+            get
+            {
+                if (LostConnectionCount > 0)
+                    return NodeStatus.Lost;
+                else if (CurrentThread == 0)
+                    return NodeStatus.Free;
+                else if (MaxThread > CurrentThread)
+                    return NodeStatus.Working;
+                else
+                    return NodeStatus.Busy;
+            }
+        }
+
+        public void Init()
+        {
+
+        }
+
+        public void HeartBeat()
+        {
+
+        }
+    }
+}
