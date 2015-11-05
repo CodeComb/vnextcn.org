db15dd701856f991a63b85aa86b014967efd6c76
あまみや ゆうこ <1@1234.sh>
2015-11-05 17:37:01 +0800

    Some logics around ci set and project

--- src/CodeComb.vNextExperimentCenter/Controllers/CIController.cs
+++ src/CodeComb.vNextExperimentCenter/Controllers/CIController.cs
@@ -50,9 +50,68 @@ namespace CodeComb.vNextExperimentCenter.Controllers
         }
 
         [HttpPost]
-        [Route("CI/Set/Build/All")]
+        [Route("CI/Set/Build/{id:Guid}")]
         [ValidateAntiForgeryToken]
-        [ClaimOrRolesAuthorize("Root, Master", "Owned CI set")]
+        [AnyRolesOrClaims("Root, Master", "Owned CI set")]
+        public async Task<IActionResult> Build(Guid id, Guid pid)
+        {
+            var project = DB.Projects
+                .Where(x => x.CISetId == id && x.Id == pid)
+                .SingleOrDefault();
+            if (project == null)
+                return Prompt(x =>
+                {
+                    x.Title = "资源没有找到";
+                    x.Details = "您请求的资源没有找到，请返回重试！";
+                    x.StatusCode = 404;
+                });
+            var status = new Status
+            {
+                ProjectId = project.Id,
+                Type = StatusType.Project,
+                UserId = User.Current.Id,
+                Result = StatusResult.Queued
+            };
+            DB.Statuses.Add(status);
+            DB.SaveChanges();
+            if (project.RunWithLinux)
+            {
+                status.RunWithLinux = true;
+                status.LinuxResult = StatusResult.Queued;
+                var node = NodeProvider.GetFreeNode(Package.OSType.Linux);
+                if (node != null)
+                    await node.SendCIBuildTask(status.Id, project.ZipUrl, project.AdditionalEnvironmentVariables);
+                else
+                    status.LinuxResult = StatusResult.Ignored;
+            }
+            if (project.RunWithOsx)
+            {
+                status.RunWithLinux = true;
+                status.OsxResult = StatusResult.Queued;
+                var node = NodeProvider.GetFreeNode(Package.OSType.OSX);
+                if (node != null)
+                    await node.SendCIBuildTask(status.Id, project.ZipUrl, project.AdditionalEnvironmentVariables);
+                else
+                    status.OsxResult = StatusResult.Ignored;
+            }
+            if (project.RunWithLinux)
+            {
+                status.RunWithWindows = true;
+                status.WindowsResult = StatusResult.Queued;
+                var node = NodeProvider.GetFreeNode(Package.OSType.Windows);
+                if (node != null)
+                    await node.SendCIBuildTask(status.Id, project.ZipUrl, project.AdditionalEnvironmentVariables);
+                else
+                    status.WindowsResult = StatusResult.Ignored;
+            }
+            DB.SaveChanges();
+            return RedirectToAction("Show", "CI", new { id = id });
+        }
+
+        [HttpPost]
+        [Route("CI/Set/Build/All/{id:Guid}")]
+        [ValidateAntiForgeryToken]
+        [AnyRolesOrClaims("Root, Master", "Owned CI set")]
         public async Task<IActionResult> BuildAll(Guid id)
         {
             var ciset = DB.CISets
@@ -107,8 +166,53 @@ namespace CodeComb.vNextExperimentCenter.Controllers
 
         [HttpGet]
         [Route("CI/Set/Edit/{id:Guid}")]
-        [ClaimOrRolesAuthorize("Root, Master", "Owned CI set")]
+        [AnyRolesOrClaims("Root, Master", "Owned CI set")]
         public IActionResult EditCISet(Guid id)
+        {
+            var ciset = DB.CISets
+                .Include(x => x.Projects)
+                .Where(x => x.Id == id)
+                .SingleOrDefault();
+            if (ciset == null)
+                return Prompt(x =>
+                {
+                    x.Title = "资源没有找到";
+                    x.Details = "您请求的资源没有找到，请返回重试！";
+                    x.StatusCode = 404;
+                });
+            return View(ciset);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("CI/Set/Edit/{id:Guid}")]
+        [AnyRolesOrClaims("Root, Master", "Owned CI set")]
+        public IActionResult EditCISet(Guid id, CISet Model)
+        {
+            var ciset = DB.CISets
+                .Include(x => x.Projects)
+                .Where(x => x.Id == id)
+                .SingleOrDefault();
+            if (ciset == null)
+                return Prompt(x =>
+                {
+                    x.Title = "资源没有找到";
+                    x.Details = "您请求的资源没有找到，请返回重试！";
+                    x.StatusCode = 404;
+                });
+            ciset.Title = Model.Title;
+            DB.SaveChanges();
+            return Prompt(x =>
+            {
+                x.Title = "修改成功";
+                x.Details = "项目集合信息已经修改成功！";
+                x.RedirectUrl = Url.Action("Show", "CI", new { id = id });
+                x.RedirectText = "项目信息";
+            });
+        }
+
+        [Route("CI/{id:Guid}")]
+        public IActionResult Show(Guid id)
         {
             var ciset = DB.CISets
                 .Include(x => x.Projects)
